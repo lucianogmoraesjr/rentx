b85bada9bf2d193558ec82912b8be1b30248c733
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2023-04-17 16:25:24 -0300

    feat: server up

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: 'Hello World... watching' })
+})
+
+app.listen(3333, () => console.log('🔥 Server running on http://localhost:3333'))
