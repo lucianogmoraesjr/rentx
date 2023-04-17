d34058087d7447c18c06f96b6325a02d14b89535
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2023-04-17 16:42:59 -0300

    chore: add eslint + prettier

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: 'Hello World... watching' })
-})
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World... watching' });
+});
 
-app.listen(3333, () => console.log('🔥 Server running on http://localhost:3333'))
+app.listen(3333, () =>
+  console.log('🔥 Server running on http://localhost:3333'),
+);
