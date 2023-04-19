6055a1e6016958d0e6d87c266ab5165acfc569de
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2023-04-19 16:06:57 -0300

    feat: add and test multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import { createCategoryController } from '../modules/cars/use-cases/create-category';
 import { listCategoriesController } from '../modules/cars/use-cases/list-categories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request, response) =>
   createCategoryController.handle(request, response),
@@ -11,5 +17,13 @@ categoriesRoutes.post('/', (request, response) =>
 categoriesRoutes.get('/', (request, response) =>
   listCategoriesController.handle(request, response),
 );
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+
+  console.log(file);
+
+  return response.send();
+});
 
 export { categoriesRoutes };
